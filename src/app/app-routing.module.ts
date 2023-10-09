ca6ab8873ff07259b1ecb41e133cc71f1f5ad96b
suriya <suriyarajasekar11@gmail.com>
2023-10-09 14:19:35 +0530

    samplewebsite

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'product',pathMatch:'full'},
+  {path:'product', component: ProductComponent},
+  {path:'cart', component: CartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
